816be4ea2ec03c8f71610637f9e527945e3ac91c
jennyf <name@example.com>
2022-12-04 21:52:57 -0800

    Day 5

---
+++ day5/solutionPt2.py
@@ -0,0 +1,43 @@
+import re
+
+def parseInitialState(initialLines):
+    stacks = []
+    stackCountLine = initialLines.pop().strip()
+    stackCount = len(re.split('\s+', stackCountLine))
+    for i in range(stackCount):
+        stacks.append([])
+    while len(initialLines) > 0:
+        stackLine = initialLines.pop()
+        for i in range(stackCount):
+            if stackLine[i * 4] != ' ':
+                stacks[i].append(stackLine[i * 4 + 1])
+    return stacks
+
+def moveCrates(stacks, line):
+    p = re.compile(r'move (?P<numMoves>\d+) from (?P<fromStack>\d+) to (?P<toStack>\d+)')
+    m = p.search(line)
+    tempStack = []
+    for i in range(int(m.group('numMoves'))):
+        tempStack.append(stacks[int(m.group('fromStack'))-1].pop())
+    for i in range(int(m.group('numMoves'))):
+        stacks[int(m.group('toStack'))-1].append(tempStack.pop())
+    return stacks
+
+inputFile = open("input", "r")
+initialLines = []
+stacks = []
+startTurns = False
+for line in inputFile:
+    if line.isspace():
+        startTurns = True
+        stacks = parseInitialState(initialLines)
+    elif startTurns:
+        stacks = moveCrates(stacks, line.strip())
+    else:
+        initialLines.append(line)
+
+result = ''
+for stack in stacks:
+    if (len(stack) > 0):
+        result += stack.pop()
+print(result)
